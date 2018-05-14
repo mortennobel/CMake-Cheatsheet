4a28ada067f233194803eb9e4f641f9c154544f6
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2018-05-14 22:51:47 +0200

    Improve C++14

---
+++ examples/example6-find/src/VulkanContext.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Morten Nobel-Jørgensen on 3/3/18.
+//
+
+#include "VulkanContext.hpp"
---
+++ examples/example6-find/src/VulkanContext.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Morten Nobel-Jørgensen on 3/3/18.
+//
+
+#ifndef HELLOPROJECT_VULKANCONTEXT_HPP
+#define HELLOPROJECT_VULKANCONTEXT_HPP
+
+
+
+class VulkanContext {
+
+};
+
+
+
+#endif //HELLOPROJECT_VULKANCONTEXT_HPP
